964c66f743ab6ca82627df12b26e5a31c37f43aa
demendezr <demendezr@yahoo.es>
2017-03-16 21:40:32 -0500

    Se agrega modelo plans para consumir los servicios del modulo 4

---
+++ app/models/plans.js
@@ -0,0 +1,21 @@
+angular.module('plansModule', []);
+angular.module('plansModule').factory("plans",function($http,url){
+    var plans = {
+        getPlans: function () {
+            return $http.get(url.get() + '/minfulness/plans/');
+        },
+        registerPlan: function(data) {
+            return $http.post(url.get() + '/minfulness/plans/',data);
+        },
+        removePlan: function (data) {
+            $http.defaults.headers.delete = "";
+            return $http.delete(url.get() + '/minfulness/plans/',
+                {
+                    data: data,
+                    headers: {'Content-Type': 'application/json'}
+                });
+        }
+    };
+    return plans;
+});
+
